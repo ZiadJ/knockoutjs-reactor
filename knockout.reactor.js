9a8f1ca7f6f92353d020e0c469e6f60a9bd8be85
ziadj <ziadjkhan@gmail.com>
2012-12-13 18:35:42 +0400

    Update knockout.reactor.js
    - Optional parameter <b>recurse</b> now accepts numerical values to specify the number of nesting levels to watch.
    - Added the <b>ko.watch</b> function.
    - Fixed a bug where the wrong callback parameters were being sent.

--- knockout.reactor.js
+++ knockout.reactor.js
@@ -3,6 +3,25 @@
 // License: MIT (http://www.opensource.org/licenses/mit-license.php)
 // Version 0.9
 
+ko.watch = function (target, options, callback) {
+    /// <summary>
+    ///     React to changes in a specific target object or function.
+    /// </summary>
+    /// <param name="target">
+    ///     The target subscribable or object/function containing subscribables.
+    /// </param>
+    /// <param name="options" type="object">
+    ///     { recurse: 1 } -> Listen to 1st level children only(default).
+    ///     { recurse: 2 } -> Listen to nested children down to the 2nd level.
+    ///     { recurse: true } -> Listen to all nested children.
+    ///     This parameter is optional.
+    /// </param>
+    /// <param name="callback" type="function">
+    ///     The callback function called when changes occur.
+    /// </param>
+    return ko.observable().watch(target, options, callback);
+}
+
 ko.subscribable.fn.watch = function (target, options, valueEvaluatorFunction) {
     /// <summary>
     ///     React to changes in a specific target object or function.
@@ -11,8 +30,10 @@ ko.subscribable.fn.watch = function (target, options, valueEvaluatorFunction) {
     ///     The target subscribable or object/function containing the subscribable(s).
     /// </param>
     /// <param name="options" type="object">
-    ///     { recurse: true } -> Listen to all nested children if any.
-    ///     This parameter can be omitted.
+    ///     { recurse: 1 } -> Listen to 1st level children only(default).
+    ///     { recurse: 2 } -> Listen to nested children down to the 2nd level.
+    ///     { recurse: true } -> Listen to all nested children.
+    ///     This parameter is optional.
     /// </param>
     /// <param name="valueEvaluatorFunction" type="function">
     ///     The evaluator or function used to update the value during changes.
@@ -30,36 +51,43 @@ ko.subscribable.fn.watch = function (target, options, valueEvaluatorFunction) {
         function watchChildren(object, recurse) {
             // Listen to all subscribables within target object.
             for (var property in object) {
-                if (recurse && typeof target[property] == 'object')
-                    watchChildren(object[property]);
-                else if (ko.isSubscribable(object[property], recurse))
+                if (recurse && (recurse === true || recurse >= 0) && typeof target[property] == 'object')
+                    watchChildren(object[property], recurse === true || Number(recurse) - 1);
+                else if (ko.isSubscribable(object[property]))
                     context.watch(object[property], options, valueEvaluatorFunction);
             }
         }
         options.targetParent = target;
         watchChildren(target, options.recurse);
-    } else if (ko.isSubscribable(target)) {
-        // For performance reasons lets use a subscription instead of a computed when targeting subscribables.
-        target.subscribe(function (targetValue) {
-            if (!context.isPaused) {
-                var returnValue = valueEvaluatorFunction.call(context, options.targetParent || targetValue);
-                if (returnValue !== undefined)
-                    context(returnValue);
-            }
-        });
     } else {
-        ko.computed(function () {
-            var targetValue = target();
-            if (!context.isPaused) {
-                // Bypass change detection for valueEvaluatorFunction during its evaluation.
-                setTimeout(function () {
-                    returnValue = valueEvaluatorFunction.call(context, options.targetParent || targetValue);
-                    // Check that a return value exists and make sure it is new before allowing an update.
-                    if (returnValue !== undefined && returnValue !== context())
+        if (options.exclude)
+            for (var i = 0 ; i < options.exclude.length; i++)
+                if (options.exclude[i] == target)
+                    return;
+
+        if (ko.isSubscribable(target)) {
+            // For performance reasons lets use a subscription instead of a computed when targeting subscribables.
+            target.subscribe(function (targetValue) {
+                if (!context.isPaused) {
+                    var returnValue = valueEvaluatorFunction.call(context, options.targetParent || target, target);
+                    if (returnValue !== undefined)
                         context(returnValue);
-                }, 0);
-            }
-        });
+                }
+            });
+        } else {
+            ko.computed(function () {
+                var targetValue = target();
+                if (!context.isPaused) {
+                    // Bypass change detection for valueEvaluatorFunction during its evaluation.
+                    setTimeout(function () {
+                        returnValue = valueEvaluatorFunction.call(context, options.targetParent || target, target);
+                        // Check that a return value exists and make sure it is new before allowing an update.
+                        if (returnValue !== undefined && returnValue !== context())
+                            context(returnValue);
+                    }, 0);
+                }
+            });
+        }
     }
 
     this.pauseWatch = function () {
@@ -72,6 +100,3 @@ ko.subscribable.fn.watch = function (target, options, valueEvaluatorFunction) {
 
     return this;
 }
-
-
-// ko.watch = function (target, options, callback) { return ko.observable().reactTo(target, options, callback); }
