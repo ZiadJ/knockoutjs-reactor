273d140ce28e7cecf0f573a605d259e22cbfc319
Ziad <hellozi@hotmail.com>
2015-11-29 12:26:50 +0400

    Support for v3.4

--- dist/ko-reactor.js
+++ dist/ko-reactor.js
@@ -194,7 +194,8 @@ ko['watch'] = function (target, options, evaluatorCallback, context) {
         case "3.1.0": subscriptionsField = 'H'; break;
         case "3.2.0": subscriptionsField = 'M'; break;
         case "3.3.0": subscriptionsField = 'G'; break;
-        default: throw "Unsupported Knockout version. Only v3.0.0 to v3.3.0 are supported when minified. Current version is " + ko.version;
+        case "3.4.0": subscriptionsField = 'K'; break;
+        default: throw "Unsupported Knockout version. Only v3.0.0 to v3.4.0 are supported when minified. Current version is " + ko.version;
     }
 
     function disposeWatcher(child) {
