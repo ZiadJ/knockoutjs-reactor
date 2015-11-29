5631e6fd990c6cfc286225da48a7d06e3d852df2
Ziad Jeeroburkhan <ziadjkhan@gmail.com>
2015-11-29 11:03:10 +0400

    Revert "Support for KO 3.4.0"

--- src/knockout.reactor.js
+++ src/knockout.reactor.js
@@ -194,8 +194,7 @@ ko['watch'] = function (target, options, evaluatorCallback, context) {
         case "3.1.0": subscriptionsField = 'H'; break;
         case "3.2.0": subscriptionsField = 'M'; break;
         case "3.3.0": subscriptionsField = 'G'; break;
-        case "3.4.0": subscriptionsField = 'K'; break;
-        default: throw "Unsupported Knockout version. Only v3.0.0 to v3.4.0 are supported when minified. Current version is " + ko.version;
+        default: throw "Unsupported Knockout version. Only v3.0.0 to v3.3.0 are supported when minified. Current version is " + ko.version;
     }
 
     function disposeWatcher(child) {
