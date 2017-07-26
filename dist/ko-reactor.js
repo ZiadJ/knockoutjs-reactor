90deb14a72604c46978d2495095b0cd8f99271e1
Martin Desrumaux <martin.desrumaux@corp.ovh.com>
2017-07-26 14:56:02 +0200

    dist

--- dist/ko-reactor.js
+++ dist/ko-reactor.js
@@ -209,7 +209,8 @@ ko['watch'] = function (target, options, evaluatorCallback, context) {
         case "3.3.0": subscriptionsField = 'G'; break;
         case "3.4.0": subscriptionsField = 'K'; break;
         case "3.4.1": subscriptionsField = 'K'; break;
-        default: throw "Unsupported Knockout version. Only v3.0.0 to v3.4.1 are supported when minified. Current version is " + ko.version;
+        case "3.4.2": subscriptionsField = 'K'; break;
+        default: throw "Unsupported Knockout version. Only v3.0.0 to v3.4.2 are supported when minified. Current version is " + ko.version;
     }
 
     function disposeWatcher(child) {
