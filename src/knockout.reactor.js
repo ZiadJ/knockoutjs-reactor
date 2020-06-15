7850e3e1f2a07ef72e3f6ef82f3bf7f255ba1b92
Stefano Bagnara <github@bago.org>
2020-06-15 17:41:51 +0200

    Add support for knockout 3.5.1

--- src/knockout.reactor.js
+++ src/knockout.reactor.js
@@ -201,6 +201,8 @@ ko['watch'] = function (target, options, evaluatorCallback, context) {
     // or the F, H or M fields when minified depending on the version used.
     // NOTE: we used to use ko.DEBUG to detect the debug versionbut it was removed in 3.4.0+,
     //       so we now check the existence of a "subscription" function.
+    // An easy way to identify the right name is to search for "{change:" in the minified 
+    // code and look at the property name assigned.
     var subscriptionsField;
     switch (typeof ko.subscription == 'function' || ko.version) {
         case true: subscriptionsField = '_subscriptions'; break;
@@ -212,7 +214,8 @@ ko['watch'] = function (target, options, evaluatorCallback, context) {
         case "3.4.1": subscriptionsField = 'K'; break;
         case "3.4.2": subscriptionsField = 'F'; break;
         case "3.5.0": subscriptionsField = 'S'; break;
-        default: throw "Unsupported Knockout version. Only v3.0.0 to v3.5.0 finals are supported when minified. Current version is " + ko.version;
+        case "3.5.1": subscriptionsField = 'U'; break;
+        default: throw "Unsupported Knockout version. Only v3.0.0 to v3.5.1 finals are supported when minified. Current version is " + ko.version;
     }
 
     function disposeWatcher(child) {
